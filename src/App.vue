8f5dc67ddc176cf31e9e169a2c7c4d9a9d58758f
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-03-31 09:32:22 +0200

    ➕ Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <script setup lang="ts">
-import { CheckboxGroupRoot, CheckboxRoot } from 'reka-ui';
+import { CheckboxGroupRoot, CheckboxRoot } from 'reka-ui'
+import { ref, toRaw } from 'vue'
 
-const fruits: string[] = []
+const options = [
+  'Apple',
+  'Banana',
+  'Cherry',
+]
+
+const fruits = ref<string[]>([])
 </script>
 
 <template>
-  <form @submit.prevent="console.log('submit')">
-    <CheckboxGroupRoot v-model="fruits">
-      <CheckboxRoot>
-        Apple
-      </CheckboxRoot>
-      
-      <CheckboxRoot>
-        Banana
-      </CheckboxRoot>
-      
-      <CheckboxRoot>
-        Cherry
+  <form 
+    class="bg-purple-50"
+    @submit.prevent="console.log('submit', toRaw(fruits))"
+   >
+    <CheckboxGroupRoot v-model="fruits" class="flex gap-x-3">
+      <CheckboxRoot 
+        v-for="option in options" 
+        :key="option"
+        class="bg-pink-200" 
+        :value="option"
+      >
+        {{ option }}
       </CheckboxRoot>
     </CheckboxGroupRoot>
 
-    <button type="submit">Submit</button>
+    <button type="submit" class="mt-8">Submit</button>
   </form>
 </template>
 
