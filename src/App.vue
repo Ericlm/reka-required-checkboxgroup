9f296d931a470d588b4358313d1420d10fa6e29f
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-03-31 09:26:17 +0200

    🚧 Checkbox group

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { CheckboxGroupRoot, CheckboxRoot } from 'reka-ui';
+
+const fruits: string[] = []
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <form @submit.prevent="console.log('submit')">
+    <CheckboxGroupRoot v-model="fruits">
+      <CheckboxRoot>
+        Apple
+      </CheckboxRoot>
+      
+      <CheckboxRoot>
+        Banana
+      </CheckboxRoot>
+      
+      <CheckboxRoot>
+        Cherry
+      </CheckboxRoot>
+    </CheckboxGroupRoot>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
